90dfa3a6b8e248327d56a4ec4c69b93c50bb4aa7
Logan Akins <lakins2@gmail.com>
2024-11-12 17:38:54 -0500

    Completed C# Story program Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
+        public static void MadLibLog()
+        {
+            Console.WriteLine("What is your name?");
+            string randomName = Console.ReadLine();
+            Console.WriteLine($"Hello, {randomName}! What is your favorite color?"); //color question
+            //color response memory
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("I'll keep that in mind! What is your favorite animal?"); //Favorite Animal
+            //Favorite Animal response memory
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("Awesome! What is your favorite band?"); //favorite Band
+            //Favorite band Memory
+            string favoriteBand = Console.ReadLine();
+            // response for last question then story production
+            Console.WriteLine($"Thank you, {randomName}! Let's make a story of your answers!");
+            Console.WriteLine($"One day {randomName} turned into a {favoriteAnimal} and realized they had magically put on a T-shirt with {favoriteBand} on it and was {favoriteColor}. What a crazy day!");
+
+
+
+        }
+
+        
+            
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLibLog();
+            
+            
         }
     }
 }
